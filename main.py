8ef6ac62972d3c75c7d01920bb6c77ae40115020
DaRkErAsEr-BeAsT <dcolonna12.dc@gmail.com>
2022-02-09 19:22:40 +0100

    fixed telegram notification

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import telebot
 import json
 import pushbullet
+# from imap_tools import MailBox
+from smtplib import SMTP_SSL, SMTP_SSL_PORT
+
 # import img_analysis
 jfile = json.load(open('credentials.json'))
 bot = telebot.TeleBot(jfile['telegram_api'])
@@ -8,25 +11,28 @@ API_KEY = jfile ['pushbullet_api']
 pb = pushbullet.Pushbullet(API_KEY)
 DICTIONNARY = [
 'monitor', 'lautsprecher', 'möbel', 'kamera', 'apparat', 'stereo', 'handy',
-'iphone', 'android', 'smartphone', 'kopfhörer'
+'iphone', 'android', 'smartphone', 'kopfhörer', 'airpods'
 ]
-# img_analysis.IMG_ANALSIS
+CHAT_ID = 1652569925
+
+
 
 @bot.message_handler(content_types=['text'])
-def echo_all(message):
+def text(message):
+    # print(message.chat.id)
     for txt in DICTIONNARY:
         if ((txt in message.text.lower())& ('suche' not in message.text.lower())):
             print(message.text)
-            pb.push_note("new item", message.text)
+            bot.send_message(CHAT_ID, message.text)
 
 @bot.message_handler(content_types=['photo'])
 def photo(message):
     cap = message.caption
-    print(cap)
+    print("photo "+cap)
     if cap != "":
         for txt in DICTIONNARY:
             if (txt in str(cap).lower()):
-                pb.push_note("new item", cap)
+                bot.send_message(CHAT_ID, cap)
     fileID = message.photo[-1].file_id
     file_info = bot.get_file(fileID)
     downloaded_file = bot.download_file(file_info.file_path)
@@ -34,6 +40,5 @@ def photo(message):
         new_file.write(downloaded_file)
     print("downloaded img")
     
-
 bot.infinity_polling()
 
